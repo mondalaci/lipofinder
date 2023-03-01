f9a423fd570a6b2dc746d474fc77aea1bac72f79
László Monda <laci@monda.hu>
2023-03-01 15:32:31 +0100

    Scrape https://www.pdbattery.com/li-polymer-battery-cells-list.html

---
+++ scrape.mjs
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+import axios from 'axios';
+import * as cheerio from 'cheerio';
+
+const batteries = [];
+
+const url = 'https://www.pdbattery.com/li-polymer-battery-cells-list.html';
+const res = await axios.get(url);
+const $ = cheerio.load(res.data);
+$('table').each((i, el) => {
+    $(el).find('tr:gt(0)').each((i, el2) => {
+        const cells = $(el2).find('td');
+        const [itemId, series, partNo, voltage, capacity, x, y, z] = cells.map((i, el) => $(el).text().trim());
+        batteries.push({
+            url,
+            capacity: +capacity,
+            x: +x,
+            y: +y,
+            z: +z,
+        });
+    });
+});
+
+console.log(batteries);
