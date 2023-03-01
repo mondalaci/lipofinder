4e1b826918b8e4152805b0df23f1e84e7d2f1891
László Monda <laci@monda.hu>
2023-03-01 16:04:25 +0100

    Scrape https://www.lipolbattery.com/LiPo-Battery-10mAh+.html

--- scrape.mjs
+++ scrape.mjs
@@ -4,9 +4,9 @@ import * as cheerio from 'cheerio';
 
 const batteries = [];
 
-const url = 'https://www.pdbattery.com/li-polymer-battery-cells-list.html';
-const res = await axios.get(url);
-const $ = cheerio.load(res.data);
+let url = 'https://www.pdbattery.com/li-polymer-battery-cells-list.html';
+let res = await axios.get(url);
+let $ = cheerio.load(res.data);
 $('table').each((i, el) => {
     $(el).find('tr:gt(0)').each((i, el2) => {
         const cells = $(el2).find('td');
@@ -17,6 +17,20 @@ $('table').each((i, el) => {
             capacity: +capacity,
             dimensions: [+x, +y, +z].sort((a,b) => a-b),
         });
+    });
+});
+
+url = 'https://www.lipolbattery.com/LiPo-Battery-10mAh+.html';
+res = await axios.get(url);
+$ = cheerio.load(res.data);
+$('table.t_list tr').each((i, el) => {
+    const cells = $(el).find('td');
+    const [partNo, capacity, dimensions, voltage] = cells.map((i, el) => $(el).text().trim());
+    batteries.push({
+        url,
+        partNo,
+        capacity: +capacity.replace(' mAh', ''),
+        dimensions: dimensions.replace(' mm', '').split(' * ').map(x => +x).sort((a,b) => a-b),
     });
 });
 
