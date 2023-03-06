1b654bac703785df3a9702f82e105a289e60fbf6
László Monda <laci@monda.hu>
2023-03-06 23:58:37 +0100

    Make search work.

--- index.mjs
+++ index.mjs
@@ -4,15 +4,16 @@ import {batteries} from './batteries.mjs';
 new Vue({
     el: '#app',
     data: {
-        x: '',
-        y: '',
-        z: '',
-        batteries: batteries,
+        x: '0',
+        y: '0',
+        z: '0',
+        batteries: [],
     },
     async created() {
-        for (const battery of this.batteries) {
+        for (const battery of batteries) {
             battery.urlHtml = battery.url.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>');
         }
+        this.search();
     },
     methods: {
         isValid() {
@@ -20,7 +21,14 @@ new Vue({
             return isNumRegex.test(this.x) && isNumRegex.test(this.y) && isNumRegex.test(this.z);
         },
         search() {
-
+            const [x, y, z] = [this.x, this.y, this.z].map(x => +x).sort((a,b) => a-b);
+            this.batteries = batteries.filter(battery => {
+                const [x1, y1, z1] = battery.dimensions;
+                return x1 >= x && y1 >= y && z1 >= z;
+            });
+            this.x = x;
+            this.y = y;
+            this.z = z;
         },
     },
 });
